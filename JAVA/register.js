1f70207cd421c638dcf3eada762051b9a3d9aeda
Matthew Valicoff <matthewvalicoff@Matthews-MacBook-Pro.local>
2021-09-23 04:56:48 -0500

    Brewfo Page, tweaks, homepage edit

---
+++ JAVA/register.js
@@ -0,0 +1,22 @@
+const toggleMen = document.querySelector(".toggleMenu");
+const navLinks = document.querySelector(".navLinks");
+const loginBut = document.querySelector(".loginBut");
+const modal = document.querySelector(".modalBG");
+const close = document.querySelector(".close");
+const userName = document.querySelector(".userName");
+const pass = document.querySelector(".pass");
+const logBut = document.querySelector(".logBut");
+const regLog = document.querySelector(".regLog");
+
+function clearInput() {
+    userName.value = "";
+    pass.value = "";
+}
+
+toggleMen.addEventListener("click", () => {navLinks.classList.toggle("active")});
+regLog.addEventListener("click", () => {modal.style.display = "flex"});
+loginBut.addEventListener("click", () => {modal.style.display = "flex"});
+close.addEventListener("click", () => {modal.style.display = "none"});
+window.addEventListener("click", (e) => {if(e.target == modal){modal.style.display = "none"}});
+window.addEventListener("click", (e) => {if(e.target == modal){clearInput()}});
+close.addEventListener("click", () => {clearInput()});
